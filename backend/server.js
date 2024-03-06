de7dd15849f077af8e3708f8906f4fd29b179cd4
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-06 22:52:20 +0530

    auth router setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoute from "./router/auth.route.js";
 
 const app = express();
 dotenv.config();
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello World..");
 });
+
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
