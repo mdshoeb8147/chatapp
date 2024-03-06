bb65027722989cbfa91e3018c16e7d881c6c7f65
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-06 23:03:53 +0530

    auth routes

--- backend/router/auth.route.js
+++ backend/router/auth.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { login, logout, signup } from "../controller/auth.controller.js";
 
 const router = express.Router();
+
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
