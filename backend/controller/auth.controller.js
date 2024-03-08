2e83b0941c67d47ef8bbcf24800523fe60903b91
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-08 22:12:20 +0530

    bcrypt js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 export const signup = async (req, res) => {
   try {
     const { fullName, username, email, password, confirmPassword, gender } =
@@ -13,7 +14,8 @@ export const signup = async (req, res) => {
       }
     }
 
-    const salt = await bcrypt(10);
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -22,7 +24,7 @@ export const signup = async (req, res) => {
       fullName,
       username,
       email,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "Male" ? boyProfilePic : girlProfilePic,
     });
