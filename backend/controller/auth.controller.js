5b9cfed4483c41568831bc145a1cf3087dd39cd6
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-08 22:00:44 +0530

    signup

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,8 +1,44 @@
+import User from "../models/user.model.js";
 export const signup = async (req, res) => {
   try {
     const { fullName, username, email, password, confirmPassword, gender } =
       req.body;
-  } catch (error) {}
+    if (password !== confirmPassword) {
+      res.status(400).json({
+        message: "Passwords do not match",
+      });
+      const user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ error: "username already taken" });
+      }
+    }
+
+    const salt = await bcrypt(10);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password,
+      gender,
+      profilePic: gender === "Male" ? boyProfilePic : girlProfilePic,
+    });
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profile,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "internal server error",
+    });
+  }
 };
 
 export const login = (req, res) => {
