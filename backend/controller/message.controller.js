242ade5f2ab04191dd0dfe6430fbc059b5d89f2e
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-12 23:10:44 +0530

    started with middle ware

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -1,1 +1,11 @@
-export const sendMessage = () => {};
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id } = req.params.id;
+    const { userId } = req.userId;
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
