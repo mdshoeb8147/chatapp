9eb7747927df8dff56a86da93e6aaec7adcf78bc
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-12 23:02:31 +0530

    message setup

--- backend/router/message.route.js
+++ backend/router/message.route.js
@@ -1,5 +1,9 @@
 import express from "express";
 
+import { sendMessage } from "../controller/message.controller.js";
+
 const router = express.Router();
+
+router.post("/send/:id", sendMessage);
 
 export default router;
