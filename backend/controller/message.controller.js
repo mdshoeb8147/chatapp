5fd5d49d638acc42ac6472b54b4601ac56dcbd17
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-14 13:08:53 +0530

    completed with middleware

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -1,8 +1,28 @@
+import Conversion from "../models/conversion.model.js";
+import Message from "../models/message.model.js";
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    const { id } = req.params.id;
-    const { userId } = req.userId;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    const conversion = await Conversion.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+    if (!conversion) {
+      conversion = await Conversion.create({
+        participants: [senderId, receiverId],
+      });
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+    if (newMessage) {
+      conversion.messages.push(newMessage._id);
+    }
+    res.status(201).json(newMessage);
   } catch (error) {
     res.status(500).json({
       error: "Internal Server Error",
