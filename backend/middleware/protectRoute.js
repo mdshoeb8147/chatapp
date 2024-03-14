636bb2c71bdc4fc37d39724741c275399365cd6d
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-14 12:57:11 +0530

    middle ware protected

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
-const protectRoute = (req, res, next) => {
+const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
     if (!token) {
@@ -8,6 +9,17 @@ const protectRoute = (req, res, next) => {
         .status(401)
         .json({ error: "unauthorized , no token provided" });
     }
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    if (!decoded) {
+      return res.status(401).json({ error: "Unauthorized : Invalid Token" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ error: "user not found" });
+    }
+    req.user = user;
+    next();
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
