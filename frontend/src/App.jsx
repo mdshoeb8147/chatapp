68385a8d047bb50084a99c9deb6aff60459a270b
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-17 16:47:11 +0530

    bg image to done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold ">App !</h1>{" "}
-    </>
+    <div className="p-4 h-screen flex items-center justify-center ">
+      <Login />
+    </div>
   );
 }
 
