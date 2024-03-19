f07de1a74d136bc31f042f14f1b452de9b296605
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-19 10:56:34 +0530

    login page completed

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,25 +1,47 @@
 const Login = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
-        <h1 className="text-3xl font-semibold text-center text-gray-300">
-          Login
-          <span className="text-blue-500 ml-3">ChatApp</span>
-        </h1>
-        <form>
-          <div>
-            <label className="label p-2">
-              <span className="text-base label-text">Username</span>
-            </label>
-            <input
-              className="w-full input input-bordered h-10"
-              type="text"
-              placeholder="Enter Username"
-            />
-          </div>
-        </form>
+    <>
+      <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+        <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
+          <h1 className="text-3xl font-semibold text-center text-gray-300">
+            Login
+            <span className="text-blue-500 ml-3">ChatApp</span>
+          </h1>
+          <form>
+            <div>
+              <label className="label p-2">
+                <span className="text-base label-text">Username</span>
+              </label>
+              <input
+                className="w-full input input-bordered h-10"
+                type="text"
+                placeholder="Enter Username"
+              />
+            </div>
+            <div>
+              <label className="label p-2">
+                <span className="text-base label-text">Password</span>
+              </label>
+              <input
+                className="w-full input input-bordered h-10"
+                type="text"
+                placeholder="Enter Password"
+              />
+            </div>
+            <a
+              href="#"
+              className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+            >
+              {"Don't"} have an account?
+            </a>
+
+            <div>
+              <button className="btn btn-block btn-sm mt-2">Login</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Login;
